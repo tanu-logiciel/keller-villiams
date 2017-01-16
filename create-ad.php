08ec2d50183d6c2dd33442da86963f21bc93887e
Mamta-logiciel <mamta.rani@logicielsolutions.co.in>
2017-01-16 10:00:27 +0530

    create ad page

---
+++ create-ad.php
@@ -0,0 +1,25 @@
+<!-- Header Part -->
+<?php include('header.php'); ?>
+
+<!-- Content Part -->
+<div class="container">
+	<div class="content-wrap business-page-custom-wrap wizard-container text-center">
+		<h1>Here you can create your Facebook Campaign</h1>
+		<h3>Fill the information below to create your first ad</h3>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="form-group">
+					<label>Campaign Type</label>
+					
+					<input type="text" name="campaign-type" placeholder="Campaign name">
+				</div>
+			</div>
+			<div class="col-sm-6">
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Footer Part -->
+<?php include('footer.php'); ?>
